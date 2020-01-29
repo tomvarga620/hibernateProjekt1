3f9ace22b2736bec22842b00132bfdc5a257221e
Tomáš Varga <tomas.varga@akademiasovy.sk>
2020-01-29 18:55:32 +0100

    227

--- hibernateMaven4/src/main/java/hibernate/Instructor.java
+++ hibernateMaven4/src/main/java/hibernate/Instructor.java
@@ -1,6 +1,8 @@
 package hibernate;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "instructor")
@@ -24,7 +26,34 @@ public class Instructor {
     @JoinColumn(name="instructor_detail_id")
     private InstructorDetail instructorDetail;
 
+
+    @OneToMany(mappedBy = "instructor",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH,CascadeType.REFRESH})
+
+    private List<Course> courses;
+
     public Instructor(){
+
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+
+    public void add(Course tempCourse){
+        if(courses == null){
+           courses = new ArrayList<>();
+        }
+
+        courses.add(tempCourse);
+
+        tempCourse.setInstructor(this);
 
     }
 
